486b87d8147c1c703db584d3381f94e3eb6344c0
pramodshejul22 <shejul.pramod22@gmail,com>
2025-11-15 21:18:01 -0700

    Python program

---
+++ python_Shubham/Arithmatic_operations.py
@@ -0,0 +1,41 @@
+#Day-3 -Shubham sir
+a=5
+b=5
+c=5
+d=5
+e=5
+f=5
+g=5
+
+a=5
+a += 2
+print(a)  #7
+
+b -= 2
+print(b)  #3
+
+c *= 2
+print(c)  #10
+
+
+d /= 2
+print(d)  #2.5
+
+e //= 2
+print(e)  #2
+
+f %= 2
+print(f) #1
+
+g **= 2
+print(g)  #25
+
+
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)    #(divide value display)
+# print(a%b)   #(Reminder display)
+# print(a**b)  #(display square root)
